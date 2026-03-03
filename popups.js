0e941f6ec31392682124df6145a23e392e70154a
Viktor L.A <jvjoaolima.9@gmail.com>
2026-03-02 21:54:35 -0300

    subindo com popups informando sobre o que se trata

---
+++ popups.js
@@ -0,0 +1,34 @@
+function abrirModal() {
+    document.getElementById("modalRenda").style.display = "flex";
+}
+
+function abrirModalAvaliacao() {
+    document.getElementById("modalAvaliacao").style.display = "flex";
+}
+
+function abrirModalValorVenda() {
+    document.getElementById("modalValorVenda").style.display = "flex";
+}
+
+function abrirModalDataNascimento() {
+    document.getElementById("modalDataNascimento").style.display = "flex";
+}
+
+function abrirModalQuantidadeCompradores() {
+    document.getElementById("modalQuantidadeCompradores").style.display = "flex";
+}
+
+function abrirModalPrazoEntrega() {
+    document.getElementById("modalPrazoEntrega").style.display = "flex";
+}
+function abrirModalInfo() {
+    document.getElementById("modalInfo").style.display = "flex";
+}
+
+
+function fecharModal() {
+    const modais = document.querySelectorAll(".modal");
+    modais.forEach(modal => {
+        modal.style.display = "none";
+    });
+}
